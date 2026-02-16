71813ecf5d8cd01fb374dcb28640843eb9110e83
Developer <developer@oopsbannerapp.com>
2026-02-16 14:11:56 +0530

    Simplify Main.java - remove comments

--- Main.java
+++ Main.java
@@ -1,18 +1,4 @@
-/**
- * OOPS Banner App - Main Class
- * 
- * @author Developer
- * @version 1.0
- * Use Case 1: Print the literal text "OOPS" to the console
- */
 public class Main {
-    
-    /**
-     * Main method - Entry point of the application
-     * Prints "OOPS" to the console
-     * 
-     * @param args Command line arguments (not used)
-     */
     public static void main(String[] args) {
         System.out.println("OOPS");
     }
