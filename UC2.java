6cb96949ecd11e07ee3db27e6e61597db25a9ead
Developer <developer@oopsbannerapp.com>
2026-02-16 15:31:05 +0530

    UC2: Implement OOPS ASCII art banner using individual print statements

---
+++ UC2.java
@@ -0,0 +1,12 @@
+public class UC2 {
+    public static void main(String[] args) {
+        // Display OOPS as an ASCII art banner using 7 lines
+        System.out.println(" ***   ***  *** *** ");
+        System.out.println("*   * *   * *   *   ");
+        System.out.println("*    ***** *** *** ");
+        System.out.println("*   * *   *   *   * ");
+        System.out.println("*   * *   * *   *   ");
+        System.out.println(" ***  *    * *  ***  ");
+        System.out.println();
+    }
+}
