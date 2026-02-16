42cea119155e58f4eb62e8553a78f1130f1a492e
Developer <developer@oopsbannerapp.com>
2026-02-16 13:48:59 +0530

    Initial commit: Java environment setup

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // This prints a message to the console
+        System.out.println("Success! Your Java environment is working.");
+        
+        // Let's do a little math to be fancy
+        int a = 10;
+        int b = 20;
+        System.out.println("The sum of " + a + " and " + b + " is: " + (a + b));
+    }
+}
